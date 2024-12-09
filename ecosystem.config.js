cf8ebf80d9e0d89a3d25d0472ab22f3e537c63ae
mzienert <mzienert@gmail.com>
2024-12-08 19:54:08 -0700

    change table name in ecosystem, using hardcoded for now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,13 +11,13 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 3000,
       AWS_REGION: 'us-west-1',
-      DYNAMODB_TABLE: 'GalvitronStack-GalvitronTable1808E743-A10U3EPBPH98'
+      DYNAMODB_TABLE: 'GalvitronTable'
     },
     env_production: {
       NODE_ENV: 'production',
       PORT: 80,
       AWS_REGION: 'us-west-1',
-      DYNAMODB_TABLE: 'GalvitronStack-GalvitronTable1808E743-A10U3EPBPH98'
+      DYNAMODB_TABLE: 'GalvitronTable'
     },
     time: true,
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
