e6a1503666b42a79a5e2374a2050d26fb3612101
mzienert <mzienert@gmail.com>
2024-12-08 21:23:33 -0700

    setting up lambda source

---
+++ lambdas/hello-world/src/index.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async () => {
+  const now = new Date();
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: 'Hello, World!',
+      timestamp: now.toISOString(),
+      datetime: now.toLocaleString(),
+    })
+  };
+};
